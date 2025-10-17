82077380555b4ae7ec8a0199f5b7d4b9dc2ef09d
jam-kaz <kazymova08@list.ru>
2025-10-17 17:09:21 +0400

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,26 @@
+s = input('Введите строку из букв латинского алфавита разных регистров:')
+for char in s:
+    if not ('a' <= char.lower() <= 'z'):
+      s = input("Введите строку корректно, по условию!:")
 
-
-if __name__ == "__main__":
-    pass # Ваш код здесь
+     
+def clean_string(input_str):
+    count_lower = {}
+    count_upper = {}
+   
+    for char in input_str:
+        if char.islower(): 
+            count_lower[char] = count_lower.get(char, 0) + 1
+        elif char.isupper(): 
+            count_upper[char.lower()] = count_upper.get(char.lower(), 0) + 1
+   
+    cleaned = ''
+    for char in input_str:
+        lower_char = char.lower()
+        if lower_char not in count_lower or lower_char not in count_upper:
+            cleaned += char
+        else:
+            if count_lower[lower_char] <= count_upper[lower_char]:
+                cleaned += char
+               
+    return cleaned
