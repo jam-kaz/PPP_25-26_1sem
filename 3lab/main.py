ab1ca70fe1dc6e1c817f17571b5e3760507c5d88
jam-kaz <kazymova08@list.ru>
2025-12-07 18:57:23 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,23 @@
-
+def evaluate(expression):
+    log = []
+    def recursive_eval(expr):
+        while '(' in expr:
+            start = expr.rfind('(')
+            end = expr.find(')', start)
+            if start != -1 and end != -1:
+                inner_expr = expr[start+1:end]
+                result = str(eval(inner_expr))
+                log.append(f'Вычисляем выражение {inner_expr} = {result}')    
+                expr = expr[:start] + result + expr[end+1:]
+        final_result = eval(expr)
+        log.append(f'Итоговый результат: {final_result}')
+        return final_result, log
+    return recursive_eval(expression)
 
 if __name__ == "__main__":
-    pass # Ваш код здесь
+    expression = input("Введите выражение: ")
+    result, steps = evaluate(expression)
+    print("путь вычислений:")
+    for step in steps:
+        print(step)
+    print(f'Окончательный результат: {result}')
